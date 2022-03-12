cdca49bcf315663135f07aa54720f044816f5453
Med Hacen <hacen13037@gmail.com>
2022-03-13 00:01:14 +0300

    Creating first version

---
+++ README.md
@@ -0,0 +1,19 @@
+# Real Time Color Detection
+This script allows the user to enter an image. The color presented 
+in this image will be detected upon the ***Hue***, ***Saturation***, and
+***Value*** selected by the user.
+
+### Example
+![example](result.gif)
+
+### Running the script
+```commandline
+python detector.py
+```
+
+### Libraries used
+- OpenCV
+- Numpy
+
+### *Author Name*
+[Mohamed El Hacen Habib](https://github.com/mohamedelhacen)
---
+++ detector.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+
+path = input("Enter image path: ")
+image = cv2.imread(path)
+width, height= image.shape[0], image.shape[1]
+
+def empty(a):
+    pass
+
+
+cv2.namedWindow("HSV")
+cv2.resizeWindow("HSV", width, height)
+cv2.createTrackbar("HUE Min", "HSV", 0, 179, empty)
+cv2.createTrackbar("HUE Max", "HSV", 179, 179, empty)
+cv2.createTrackbar("SAT Min", "HSV", 0, 255, empty)
+cv2.createTrackbar("SAT Max", "HSV", 255, 179, empty)
+cv2.createTrackbar("VALUE Min", "HSV", 0, 255, empty)
+cv2.createTrackbar("VALUE Max", "HSV", 255, 255, empty)
+
+while True:
+    imageHSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    h_min = cv2.getTrackbarPos("HUE Min", "HSV")
+    h_max = cv2.getTrackbarPos("HUE Max", "HSV")
+    s_min = cv2.getTrackbarPos("SAT Min", "HSV")
+    s_max = cv2.getTrackbarPos("SAT Max", "HSV")
+    v_min = cv2.getTrackbarPos("VALUE Min", "HSV")
+    v_max = cv2.getTrackbarPos("VALUE Max", "HSV")
+
+    lower = np.array([h_min, s_min, v_min])
+    upper = np.array([h_max, s_max, v_max])
+    mask = cv2.inRange(imageHSV, lower, upper)
+    result = cv2.bitwise_and(image, image, mask=mask)
+
+    mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+    output = np.hstack((image, mask, result))
+    cv2.imshow("Output", output)
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+
+cv2.destroyAllWindows()
